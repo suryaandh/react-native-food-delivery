48dccc19fd2edd51841b2cded0d677e1b73e9c1f
suryaandh <gdswiyasa@gmail.com>
2023-11-17 16:02:10 +0800

    feat : add custom header

---
+++ components/CustomHeader.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+
+const SearchBar = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchSection}>
+        <View style={styles.searchField}>
+          <Ionicons
+            name="ios-search"
+            style={styles.searchIcon}
+            size={20}
+            color={Colors.medium}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Restaurants, groceries, dishes"
+            placeholderTextColor={Colors.medium}
+          />
+        </View>
+        <Link href={"/"} asChild>
+          <TouchableOpacity style={styles.optionButton}>
+            <Ionicons name="options-outline" size={20} color={Colors.primary} />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Image
+            style={styles.bike}
+            source={require("@/assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.titleContainer}>
+          <Text style={styles.title}>Delivery · Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>San Francisco</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  container: {
+    height: 60,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  bike: {
+    width: 30,
+    height: 30,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  locationName: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  searchContainer: {
+    height: 50,
+    backgroundColor: "#fff",
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+    alignItem: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+});
+
+export default CustomHeader;
